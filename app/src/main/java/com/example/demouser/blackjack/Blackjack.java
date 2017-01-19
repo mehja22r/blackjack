2fe213b2e5b0d1eaf0268012ae443685a81bf2ab
hyeyoonj <demouser@n227-224.mtholyoke.edu>
2017-01-19 10:27:35 -0500

    blackjack

--- app/src/main/java/com/example/demouser/blackjack/Blackjack.java
+++ app/src/main/java/com/example/demouser/blackjack/Blackjack.java
@@ -1,6 +1,7 @@
 package com.example.demouser.blackjack;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 /**
  * Created by demouser on 1/13/17.
@@ -16,11 +17,19 @@ public class Blackjack {
     int bet;
     final int MAX_BET = 500;
     final int MIN_BET = 10;
+    int currentBet;
+
+    //cannot exceed 5
+    int hitCount;
+    boolean doubleBetBoolean;
 
 
     public Blackjack() {
         deck = new Deck();
         bet = 1000;
+
+        hitCount = 0;
+        doubleBetBoolean = false;
     }
 
     public void startGame() {
@@ -36,28 +45,138 @@ public class Blackjack {
 
     }
 
+    public void checkTurn() {
+        //dealer wins
+        if (playerScore()>21 && dealerScore() <=21) {
+            dealerWins();
+        }
+        //player wins
+        else if (dealerScore() > 21 && playerScore() <= 21) {
+            playerWins();
+        }
+    }
+
     public void betGame(int betAmount) {
+        currentBet = betAmount;
         bet = bet - betAmount;
     }
 
     public void stand() {
+        checkTurn();
+        dealerTurn();
+        int pScore  = 21 - playerScore();
+        int dScore = 21 - dealerScore();
+
+        if (pScore>dScore) {
+            dealerWins();
+        }
+        else if (dScore > pScore) {
+            playerWins();
+        }
+        //what if score is the same
+        else {
+            tie();
+
+        }
 
     }
 
-    public void hit() {}
+    public void tie() {
+        bet = bet + currentBet;
+    }
+
+    public void dealerWins() {
+    }
+
+    public void playerWins() {
+        bet += currentBet *2;
+    }
+
+
+    public void hit() {
+        checkTurn();
+        if (hitCount < 5) {
+            playerCards.add(deck.drawCard());
+            hitCount++;
+        }
+        else {
+            System.out.println("Cannot hit more than five times");
+        }
+    }
 
     public void doubleBet() {
-
+        checkTurn();
+        doubleBetBoolean = true;
+        bet = bet * 2;
+        playerCards.add(deck.drawCard());
+        stand();
     }
 
-    public void split() {}
+    public void dealerTurn() {
+        while (dealerScore() < 16) {
+            dealerCards.add(deck.drawCard());
+        }
+    }
 
     public ArrayList<Card> getPlayerCards() {
+
         return playerCards;
     }
 
     public ArrayList<Card> getDealerCards() {
+
         return dealerCards;
     }
 
+    public int playerScore() {
+        int playerTotal = 0;
+
+        for (int i = 0; i < playerCards.size(); i++) {
+            int realValues = playerCards.get(i).getValue();
+
+            if (realValues <= 8) {
+                playerTotal += realValues + 2;
+            }
+            else if (realValues >= 9 && realValues <= 11)
+                playerTotal += 10;
+            else {
+                int temp = 21-playerTotal;
+                if (temp <= 10) {
+                    realValues = 1;
+                    playerTotal += realValues;
+                }
+                else {
+                    realValues = 11;
+                    playerTotal += realValues;
+                }
+            }
+        }
+        return playerTotal;
+    }
+
+    public int dealerScore() {
+        int dealerTotal = 0;
+
+        for (int i = 0; i < dealerCards.size(); i++) {
+            int realValues = dealerCards.get(i).getValue();
+
+            if (realValues <= 8) {
+                dealerTotal += realValues + 2;
+            }
+            else if (realValues >= 9 && realValues <= 11)
+                dealerTotal += 10;
+            else {
+                int temp = 21-dealerTotal;
+                if (temp <= 10) {
+                    realValues = 1;
+                    dealerTotal += realValues;
+                }
+                else {
+                    realValues = 11;
+                    dealerTotal += realValues;
+                }
+            }
+        }
+        return dealerTotal;
+    }
 }
--- app/src/main/java/com/example/demouser/blackjack/MainActivity.java
+++ app/src/main/java/com/example/demouser/blackjack/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Blackjack blackjack;
+    private Blackjack blackjack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +21,164 @@ public class MainActivity extends AppCompatActivity {
         int cardShape = blackjack.getPlayerCards().get(0).getShape();
 
         ImageView p1 = ((ImageView) findViewById(R.id.p1));
-        if (cardValue == 0 && cardShape == 0) {
-            p1.setImageResource(R.drawable.c2);
+        ImageView p2 = ((ImageView) findViewById(R.id.p2));
+        ImageView p3 = ((ImageView) findViewById(R.id.p3));
+        ImageView p4 = ((ImageView) findViewById(R.id.p4));
+        ImageView p5 = ((ImageView) findViewById(R.id.p5));
+        ImageView p6 = ((ImageView) findViewById(R.id.p6));
+
+        ImageView d1 = ((ImageView) findViewById(R.id.d1));
+        ImageView d2 = ((ImageView) findViewById(R.id.d2));
+        ImageView d3 = ((ImageView) findViewById(R.id.d3));
+        ImageView d4 = ((ImageView) findViewById(R.id.d4));
+        ImageView d5 = ((ImageView) findViewById(R.id.d5));
+        //for loop
+        for (int i = 0; i < blackjack.getPlayerCards().size(); i++) {
+            cardValue = blackjack.getPlayerCards().get(i).getValue();
+            cardShape = blackjack.getPlayerCards().get(i).getShape();
+
+            if (i ==1) {
+                showCard(p2, cardValue, cardShape);
+            }
+            else if (i == 2) {
+                showCard(p3, cardValue, cardShape);
+            }
+            else if (i == 3) {
+                showCard(p4, cardValue, cardShape);
+            }
+            else if (i==4) {
+                showCard(p5, cardValue, cardShape);
+            }
+            else if (i==5) {
+                showCard(p6, cardValue, cardShape);
+            }
+            else if (i==6) {
+                showCard(p6, cardValue, cardShape);
+            }
+        }
+
+        for (int j = 0;j < blackjack.getDealerCards().size(); j++) {
+            cardValue = blackjack.getDealerCards().get(j).getValue();
+            cardShape = blackjack.getDealerCards().get(j).getShape();
+
+            if (j==1)
+                showCard(d1, cardValue, cardShape);
+            else if (j==2)
+                showCard(d2, cardValue, cardShape);
+            else if (j==3)
+                showCard(d3, cardValue, cardShape);
+            else if (j==4)
+                showCard(d4, cardValue, cardShape);
+            else if (j==5)
+                showCard(d5, cardValue, cardShape);
         }
     }
+
+        public void showCard(ImageView p1, int cardValue, int cardShape) {
+        if (cardValue == 0 && cardShape == 0)
+            p1.setImageResource(R.drawable.c2);
+        else if (cardValue==0 && cardShape==1)
+            p1.setImageResource(R.drawable.d2);
+        else if (cardValue==0 && cardShape==2)
+            p1.setImageResource(R.drawable.h2);
+        else if (cardValue==0 && cardShape==3)
+            p1.setImageResource(R.drawable.s2);
+        else if (cardValue==1 && cardShape==0)
+            p1.setImageResource(R.drawable.c3);
+        else if (cardValue==1 && cardShape==1)
+            p1.setImageResource(R.drawable.d3);
+        else if (cardValue==1 && cardShape==2)
+            p1.setImageResource(R.drawable.h3);
+        else if (cardValue==1 && cardShape==3)
+            p1.setImageResource(R.drawable.s3);
+        else if (cardValue==2 && cardShape==0)
+            p1.setImageResource(R.drawable.c4);
+        else if (cardValue==2 && cardShape==1)
+            p1.setImageResource(R.drawable.d4);
+        else if (cardValue==2 && cardShape==2)
+            p1.setImageResource(R.drawable.h4);
+        else if (cardValue==2 && cardShape==3)
+            p1.setImageResource(R.drawable.s4);
+        else if (cardValue==3 && cardShape==0)
+            p1.setImageResource(R.drawable.c5);
+        else if (cardValue==3 && cardShape==1)
+            p1.setImageResource(R.drawable.d5);
+        else if (cardValue==3 && cardShape==2)
+            p1.setImageResource(R.drawable.h5);
+        else if (cardValue==3 && cardShape==3)
+            p1.setImageResource(R.drawable.s5);
+        else if (cardValue==4 && cardShape==0)
+            p1.setImageResource(R.drawable.c6);
+        else if (cardValue==4 && cardShape==1)
+            p1.setImageResource(R.drawable.d6);
+        else if (cardValue==4 && cardShape==2)
+            p1.setImageResource(R.drawable.h6);
+        else if (cardValue==4 && cardShape==3)
+            p1.setImageResource(R.drawable.s6);
+        else if (cardValue==5 && cardShape==0)
+            p1.setImageResource(R.drawable.c7);
+        else if (cardValue==5 && cardShape==1)
+            p1.setImageResource(R.drawable.d7);
+        else if (cardValue==5 && cardShape==2)
+            p1.setImageResource(R.drawable.h7);
+        else if (cardValue==5 && cardShape==3)
+            p1.setImageResource(R.drawable.s7);
+        else if (cardValue==6 && cardShape==0)
+            p1.setImageResource(R.drawable.c8);
+        else if (cardValue==6 && cardShape==1)
+            p1.setImageResource(R.drawable.d8);
+        else if (cardValue==6 && cardShape==2)
+            p1.setImageResource(R.drawable.h8);
+        else if (cardValue==6 && cardShape==3)
+            p1.setImageResource(R.drawable.s8);
+        else if (cardValue==7 && cardShape==0)
+            p1.setImageResource(R.drawable.c9);
+        else if (cardValue==7 && cardShape==1)
+            p1.setImageResource(R.drawable.d9);
+        else if (cardValue==7 && cardShape==2)
+            p1.setImageResource(R.drawable.h9);
+        else if (cardValue==7 && cardShape==3)
+            p1.setImageResource(R.drawable.s9);
+        else if (cardValue==8 && cardShape==0)
+            p1.setImageResource(R.drawable.c10);
+        else if (cardValue==8 && cardShape==1)
+            p1.setImageResource(R.drawable.d10);
+        else if (cardValue==8 && cardShape==2)
+            p1.setImageResource(R.drawable.h10);
+        else if (cardValue==8 && cardShape==3)
+            p1.setImageResource(R.drawable.s10);
+        else if (cardValue==9 && cardShape==0)
+            p1.setImageResource(R.drawable.jc);
+        else if (cardValue==9 && cardShape==1)
+            p1.setImageResource(R.drawable.jd);
+        else if (cardValue==9 && cardShape==2)
+            p1.setImageResource(R.drawable.jh);
+        else if (cardValue==9 && cardShape==3)
+            p1.setImageResource(R.drawable.js);
+        else if (cardValue==10 && cardShape==0)
+            p1.setImageResource(R.drawable.qc);
+        else if (cardValue==10 && cardShape==1)
+            p1.setImageResource(R.drawable.qd);
+        else if (cardValue ==10 && cardShape==2)
+            p1.setImageResource(R.drawable.qh);
+        else if (cardValue==10 && cardShape==3)
+            p1.setImageResource(R.drawable.qs);
+        else if (cardValue==11 && cardShape==0)
+            p1.setImageResource(R.drawable.kc);
+        else if (cardValue==11 && cardShape==1)
+            p1.setImageResource(R.drawable.kd);
+        else if (cardValue==11 && cardShape==2)
+            p1.setImageResource(R.drawable.kh);
+        else if (cardValue==11 && cardShape==3)
+            p1.setImageResource(R.drawable.ks);
+        else if (cardValue==12 && cardShape==0)
+            p1.setImageResource(R.drawable.ac);
+        else if (cardValue==12 && cardShape==1)
+            p1.setImageResource(R.drawable.ad);
+        else if (cardValue==12 && cardShape==2)
+            p1.setImageResource(R.drawable.ah);
+        else if (cardValue==12 && cardShape==3)
+            p1.setImageResource(R.drawable.as);
+    }
 }
+
