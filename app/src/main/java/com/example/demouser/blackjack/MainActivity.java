8c7aa633d221f137d984ab4bedd5d76eb70d7b5f
mehja22r <mehja22r@mtholyoke.edu>
2017-01-19 15:12:24 -0500

    More changes

--- app/src/main/java/com/example/demouser/blackjack/MainActivity.java
+++ app/src/main/java/com/example/demouser/blackjack/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.demouser.blackjack;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
